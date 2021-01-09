8edd1449ec6d632ad4df86e04c3f3814ceb09847
Hamish Arro <Hamisharro@icloud.com>
2021-01-09 15:52:16 +0000

    made should accept with one variable test pass

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,12 +2,16 @@ require 'plane'
 
 describe Plane do
 
-  it "should be able to land" do
-    expect(subject).to respond_to :land
-  end
+  describe "#land" do
 
-  it "should accept aiport" do
-    expect(subject).to respond_to(:land).with(1).arguments
+    it "should be able to land" do
+      expect(subject).to respond_to :land
+    end
+
+    it "should accept aiport" do
+      expect(subject).to respond_to(:land).with(1).arguments
+    end
+
   end
 
 end
