cc5339142306e2fb6a5cb7bec40d2601e7e1578e
Hamish Arro <Hamisharro@icloud.com>
2021-01-09 15:33:14 +0000

    made land test pass

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,8 +2,8 @@ require 'plane'
 
 describe Plane do
 
-  # it "should be able to land" do
-  #   expect(subject).to respond_to :land
-  # end
+  it "should be able to land" do
+    expect(subject).to respond_to :land
+  end
 
 end
